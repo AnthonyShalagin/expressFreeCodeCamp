f16a9143598e4e404a6d5443f7f3fd3b70f8333a
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-04-12 11:05:54 -0400

    callback example

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express'),
       app = express();
 
 app.get('/',(request,response)=>{
-  response.send(‘Hello world’);
+  response.send('Hello world');
 });
 
 //Binding the server to a port(3000)
-app.listen(3000,()=>console.log(‘express server started at port 300’));
+app.listen(3000,()=>console.log('express server started at port 300'));
+
+
+
+/*
+const express = require('express'),
+      app = express();
+function sayHello(request,response,next){
+  console.log(‘I must be called’);
+  next();
+}
+app.get('/', sayHello, (request, response)=>{
+  response.send('sayHello');
+});
+app.listen(3000,()=>console.log('Express server started at port 3000'));
+
+*/
