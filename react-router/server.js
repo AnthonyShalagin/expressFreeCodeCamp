cd1b738e3844ad6f54adf6b856a6966cc60e41fa
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-04-13 14:18:08 -0400

    More modular routing

--- react-router/server.js
+++ react-router/server.js
@@ -1,16 +1,12 @@
 const express = require('express'),
       app = express();
 
-//requiring the basic_router.js
-app.use('/users',require('.react-router/basic_router'));
+app.use('/users',require('./basic_router'));
+                         
+app.use('/api',require('./api_route'));
 
-//routes
 app.get('/posts/newpost',(request,response)=>{
-  response.send('new post');
-});
-
-app.get('/api',(request,response)=>{
-  response.send('API route');
+   response.send('new post');
 });
 
 app.listen(3000,()=>console.log('Express server started at port 3000'));
