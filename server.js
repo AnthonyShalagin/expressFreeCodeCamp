35e989bbd26020678568105567f54455803c9d6a
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-04-13 09:37:34 -0400

    request params example

--- server.js
+++ server.js
@@ -1,25 +1,7 @@
 const express = require('express'),
       app = express();
-
-app.get('/',(request,response)=>{
-  response.send('Hello world');
+app.get('/:name/:age', (request, response)=>{
+   response.send(request.params);
 });
 
-//Binding the server to a port(3000)
-app.listen(3000,()=>console.log('express server started at port 300'));
-
-
-
-/*
-const express = require('express'),
-      app = express();
-function sayHello(request,response,next){
-  console.log(‘I must be called’);
-  next();
-}
-app.get('/', sayHello, (request, response)=>{
-  response.send('sayHello');
-});
 app.listen(3000,()=>console.log('Express server started at port 3000'));
-
-*/
